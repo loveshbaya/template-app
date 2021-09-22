f2a0e7c1a978d20b8b61838c18f2cadd5e17eed0
Sumit Suthar <sumitsuthar94@gmail.com>
2021-09-23 00:12:53 +0530

    UPI Id page

--- src/screens/UpiPay.tsx
+++ src/screens/UpiPay.tsx
@@ -2,7 +2,7 @@ import React, {useCallback} from 'react';
 import {Platform, Linking} from 'react-native';
 import {useNavigation} from '@react-navigation/core';
 
-import {Block, Button, Image, Text} from '../components';
+import {Block, Button, Image, Text, gradients, Input} from '../components';
 import {useData, useTheme, useTranslation} from '../hooks';
 
 const isAndroid = Platform.OS === 'android';
@@ -11,10 +11,38 @@ const UpiPay = () => {
   const {t} = useTranslation();
   const navigation = useNavigation();
   const {assets, colors, sizes} = useTheme();
-  console.log("hello in UpiPay")
   return (
-    <Block safe marginTop={sizes.md}>
+    <Block safe marginTop={sizes.md} marginBottom={sizes.md} paddingHorizontal={sizes.padding}>
+    <Block scroll showsVerticalScrollIndicator={true}>
+        <Block card marginTop={sizes.sm} >
+            <Text
+                  h5
+                  bold
+                  marginTop={sizes.sm}
+                  marginLeft={sizes.xs}>
+                  Enter UPI ID
+                </Text>
+            <Text
+              p
+              marginTop={sizes.s}
+              marginLeft={sizes.xs}
+              marginBottom={sizes.sm}>
+              Pay someone using UPI.
+            </Text>
+            <Input placeholder="UPI ID" marginBottom={sizes.sm} />
+            <Block row  justify='space-around' paddingHorizontal={sizes.sm}>
+              <Button color={colors.primaryBlue} marginBottom={sizes.base} >
+                <Block row  justify='space-between' paddingHorizontal={sizes.sm} align='center'>
+                    <Text white bold transform="uppercase" >
+                      Verify
+                    </Text>
+                </Block>
+                
+            </Button>
+          </Block>
+        </Block>
     </Block>
+</Block>
   );
 };
 
