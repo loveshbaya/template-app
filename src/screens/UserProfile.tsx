55279d9248ee270ed53f4d09cca21a62b6feb1c8
Sumit Suthar <sumitsuthar94@gmail.com>
2021-09-29 23:45:15 +0530

    user profile image

--- src/screens/UserProfile.tsx
+++ src/screens/UserProfile.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import {Platform} from 'react-native';
 import {useNavigation} from '@react-navigation/core';
 
-import {Block, Text} from '../components/';
-import {useTheme, useTranslation} from '../hooks/';
+import {Block, Text,Image} from '../components/';
+import {useData, useTheme, useTranslation} from '../hooks/';
 
 const isAndroid = Platform.OS === 'android';
 
 const UserProfile = () => {
     const {t} = useTranslation();
     const navigation = useNavigation();
+    const {user} = useData();
     const {assets, colors, sizes, fonts, weights} = useTheme();
     console.log("On User profile")
     return (
@@ -18,7 +19,7 @@ const UserProfile = () => {
                 scroll
                 showsVerticalScrollIndicator={false}
                 contentContainerStyle={{paddingVertical: sizes.padding}}>
-                <Block row>
+                <Block row >
                     <Block card radius={1}>
                         <Block padding={sizes.s} justify="space-between">
                             <Text font={fonts.p} size={sizes.h5} weight={weights.p} h4>
@@ -32,17 +33,36 @@ const UserProfile = () => {
                             </Text>
                         </Block>
                     </Block>
-                </Block>
-                <Block row marginTop={sizes.sm}>
-                    <Text style={{width: '100%'}} h5 marginLeft={sizes.md}>
-                        Lovesh Baya
-                    </Text>
-                    <Text p marginLeft={sizes.md}>
-                        loves.baya
-                    </Text>
+
+                <Block card radius={1} align='center' >
+                    <Block padding={sizes.s} justify="center">
+                            <Image
+                                width={70}
+                                height={70}
+                                source={{uri: user?.avatar}}
+                                radius={100}
+                            />
+                        </Block>
                 </Block>
             </Block>
+
+            <Block card row marginTop={sizes.m} justify='space-between'>
+                    <Text p font={fonts.p} size={sizes.h5} weight={weights.p} h4>
+                                Wallet Balance
+                    </Text>
+                    <Text p font={fonts.p} size={sizes.h6} weight={weights.p} h5 marginRight={sizes.m}>
+                               15 INR
+                    </Text>
+            </Block>
+
+            <Block card row marginTop={sizes.m} justify='space-between'>
+                    <Text p font={fonts.p} size={sizes.h5} weight={weights.p} h4>
+                                CashBack History
+                    </Text>
+                    
+            </Block>     
         </Block>
+    </Block>
     );
 };
 
