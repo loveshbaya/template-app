bbcc476e5911c1edb0a4afb4834c9eea06f29b9e
loveshbaya <lovesh.baya@gmail.com>
2021-10-20 16:31:44 +0530

    services on home page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -5,7 +5,15 @@ import {Block, Button, Image, Text} from '../components/';
 import {AntDesign, Ionicons} from "@expo/vector-icons";
 import {useNavigation} from "@react-navigation/core";
 
-import {faAngleRight} from "@fortawesome/free-solid-svg-icons";
+import {
+    faAngleRight,
+    faClone,
+    faCoffee,
+    faDumbbell,
+    faHotel,
+    faPills,
+    faUtensils
+} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
 import CardFlip from "react-native-card-flip";
 
@@ -104,7 +112,47 @@ const Home = () => {
                           Services
                       </Text>
                   </Block>
-                  <Block row justify="space-evenly" marginLeft={sizes.sm} marginRight={sizes.sm}>
+                  <Block marginLeft={sizes.sm} marginRight={sizes.sm}>
+                      <Block row justify={"space-evenly"} marginBottom={sizes.m}>
+                          <Block style={{maxWidth: 65}} align={"center"}>
+                              <FontAwesomeIcon size={sizes.md} icon={faCoffee} color={colors.icon}></FontAwesomeIcon>
+                              <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
+                                  Cafe
+                              </Text>
+                          </Block>
+                          <Block style={{maxWidth: 65}} align={"center"}>
+                              <FontAwesomeIcon size={sizes.md} icon={faDumbbell} color={colors.icon}></FontAwesomeIcon>
+                              <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
+                                  GYM
+                              </Text>
+                          </Block>
+                          <Block style={{maxWidth: 65}} align={"center"}>
+                              <FontAwesomeIcon size={sizes.md} icon={faUtensils} color={colors.icon}></FontAwesomeIcon>
+                              <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
+                                  Restaurants
+                              </Text>
+                          </Block>
+                      </Block>
+                      <Block row justify={"space-evenly"} marginBottom={sizes.m}>
+                          <Block style={{maxWidth: 65}} align={"center"}>
+                              <FontAwesomeIcon size={sizes.md} icon={faHotel} color={colors.icon}></FontAwesomeIcon>
+                              <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
+                                  Hotels
+                              </Text>
+                          </Block>
+                          <Block style={{maxWidth: 65}} align={"center"}>
+                              <FontAwesomeIcon size={sizes.md} icon={faPills} color={colors.icon}></FontAwesomeIcon>
+                              <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
+                                  Pharmacy
+                              </Text>
+                          </Block>
+                          <Block style={{maxWidth: 65}} align={"center"}>
+                              <FontAwesomeIcon size={sizes.md} icon={faClone} color={colors.icon}></FontAwesomeIcon>
+                              <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
+                                  more...
+                              </Text>
+                          </Block>
+                      </Block>
 
                   </Block>
               </Block>
