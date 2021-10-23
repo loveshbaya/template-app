5f806d3f7eb3e10c0fa580dba57eb8ef264597ed
loveshbaya <lovesh.baya@gmail.com>
2021-10-23 13:43:06 +0530

    Services page

--- src/screens/Services.tsx
+++ src/screens/Services.tsx
@@ -1,14 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Platform} from 'react-native';
 import {useNavigation} from '@react-navigation/core';
 
 import {useTheme, useTranslation} from '../hooks/';
-import {Block, Button, Image, Text} from '../components/';
-import {AntDesign, Ionicons} from "@expo/vector-icons";
+import {Block, Text} from '../components/';
 
 import {
-    faAngleRight,
-    faClone,
     faCoffee,
     faDumbbell,
     faGift,
@@ -16,7 +13,7 @@ import {
     faPills,
     faShoePrints,
     faShoppingBag,
-    faShoppingBasket,
+    faStore,
     faTshirt,
     faUtensils
 } from "@fortawesome/free-solid-svg-icons";
@@ -33,76 +30,97 @@ const Services = () => {
 
   return (
       <Block safe>
-        <Block row marginTop={sizes.m}  radius={1}>
+          <Block scroll showsVerticalScrollIndicator={true}
+                 contentContainerStyle={{paddingVertical: sizes.padding}}>
+              <Block card radius={1} align={"center"}>
+                  <Block row marginTop={sizes.m} radius={1}>
                   <Block marginLeft={sizes.sm} marginRight={sizes.sm}>
                       <Block row justify={"space-evenly"} marginBottom={sizes.m}>
                           <Block style={{maxWidth: 65}} align={"center"}>
-                              <FontAwesomeIcon size={sizes.md} icon={faCoffee} color={colors.icon}></FontAwesomeIcon>
+                              <FontAwesomeIcon size={sizes.md} icon={faCoffee}></FontAwesomeIcon>
                               <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
                                   Cafe
                               </Text>
                           </Block>
                           <Block style={{maxWidth: 65}} align={"center"}>
-                              <FontAwesomeIcon size={sizes.md} icon={faDumbbell} color={colors.icon}></FontAwesomeIcon>
+                              <FontAwesomeIcon size={sizes.md} icon={faDumbbell}></FontAwesomeIcon>
                               <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
                                   GYM
                               </Text>
                           </Block>
                           <Block style={{maxWidth: 65}} align={"center"}>
-                              <FontAwesomeIcon size={sizes.md} icon={faUtensils} color={colors.icon}></FontAwesomeIcon>
+                              <FontAwesomeIcon size={sizes.md} icon={faUtensils}></FontAwesomeIcon>
                               <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
                                   Restaurants
                               </Text>
                           </Block>
                           <Block style={{maxWidth: 65}} align={"center"}>
-                              <FontAwesomeIcon size={sizes.md} icon={faHotel} color={colors.icon}></FontAwesomeIcon>
+                              <FontAwesomeIcon size={sizes.md} icon={faHotel}></FontAwesomeIcon>
                               <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
                                   Hotels
                               </Text>
                           </Block>
-                          <Block style={{maxWidth: 65}} align={"center"}>
-                              <FontAwesomeIcon size={sizes.md} icon={faPills} color={colors.icon}></FontAwesomeIcon>
-                              <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
-                                  Pharmacy
-                              </Text>
-                          </Block>
+
                       </Block>
-                      
+
                       <Block row justify={"space-evenly"} marginBottom={sizes.m}>
                           <Block style={{maxWidth: 65}} align={"center"}>
-                              <FontAwesomeIcon size={sizes.md} icon={faShoePrints} color={colors.icon}></FontAwesomeIcon>
+                              <FontAwesomeIcon size={sizes.md} icon={faShoePrints}></FontAwesomeIcon>
                               <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
                                   Shoes
                               </Text>
                           </Block>
                           <Block style={{maxWidth: 65}} align={"center"}>
-                              <FontAwesomeIcon size={sizes.md} icon={faTshirt} color={colors.icon}></FontAwesomeIcon>
+                              <FontAwesomeIcon size={sizes.md} icon={faTshirt}></FontAwesomeIcon>
                               <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
                               Clothes
                               </Text>
                           </Block>
                           <Block style={{maxWidth: 65}} align={"center"}  >
-                              <FontAwesomeIcon size={sizes.md} icon={faShoppingBag} color={colors.icon} onPress={() => navigation.navigate('Services')} ></FontAwesomeIcon>
+                              <FontAwesomeIcon size={sizes.md} icon={faShoppingBag}></FontAwesomeIcon>
                               <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
                               Accessories
                               </Text>
                           </Block>
                           <Block style={{maxWidth: 65}} align={"center"}  >
-                              <FontAwesomeIcon size={sizes.md} icon={faGift} color={colors.icon} onPress={() => navigation.navigate('Services')} ></FontAwesomeIcon>
+                              <FontAwesomeIcon size={sizes.md} icon={faGift}></FontAwesomeIcon>
                               <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
                               Gifts
                               </Text>
                           </Block>
-                          <Block style={{maxWidth: 65}} align={"center"}  >
-                              <FontAwesomeIcon size={sizes.md} icon={faGift} color={colors.icon} onPress={() => navigation.navigate('Services')} ></FontAwesomeIcon>
+
+                      </Block>
+                      <Block row justify={"space-evenly"} marginBottom={sizes.m}>
+                          <Block style={{maxWidth: 65}} align={"center"}>
+                              <FontAwesomeIcon size={sizes.md} icon={faShoppingBag}></FontAwesomeIcon>
                               <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
-                              Gifts
+                                  Accessories
+                              </Text>
+                          </Block>
+                          <Block style={{maxWidth: 65}} align={"center"}>
+                              <FontAwesomeIcon size={sizes.md} icon={faGift}></FontAwesomeIcon>
+                              <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
+                                  Gifts
+                              </Text>
+                          </Block>
+                          <Block style={{maxWidth: 65}} align={"center"}>
+                              <FontAwesomeIcon size={sizes.md} icon={faPills}></FontAwesomeIcon>
+                              <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
+                                  Pharmacy
+                              </Text>
+                          </Block>
+                          <Block style={{maxWidth: 65}} align={"center"}>
+                              <FontAwesomeIcon size={sizes.md} icon={faStore}></FontAwesomeIcon>
+                              <Text align={"center"} marginTop={sizes.s} lineHeight={17} font={fonts.p} size={11} p>
+                                  Others
                               </Text>
                           </Block>
                       </Block>
 
                   </Block>
               </Block>
+              </Block>
+          </Block>
       </Block>
   );
 };
